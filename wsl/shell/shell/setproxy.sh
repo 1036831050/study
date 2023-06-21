1009205d58fc22ea861b3b59c253dac87a84ab60
lmd <1036831050@qq.com>
2023-06-21 20:04:53 +0800

    整理wsl文件内容

--- wsl/shell/shell/setproxy.sh
+++
@@ -1,2 +0,0 @@
-export http_proxy=http://192.168.1.105:10809
-export https_proxy=http://192.168.1.105:10809
--- wsl/shell/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $# -ne 0 ];then
-    if [ "$1" == "lmd" ];then
-        echo "lmd";
-        [ -e /Users/v_liangmingdong/Desktop/shell ] || echo "caros";
-    fi
-fi
