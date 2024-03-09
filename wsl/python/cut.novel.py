0200e60a38370f2ddd048ba6d40c61cf0b4adb95
lmd <1036831050@qq.com>
2024-03-09 20:29:21 +0800

    wsl提交

--- wsl/python/cut.novel.py
+++ wsl/python/cut.novel.py
@@ -1,93 +1,84 @@
 import re
 import os
-def splitnovel(file_path):
-    with open(file_path,'rb',encoding='utf-8')as f1:
-        content = f1.readlines();
-        return content;
-'''
-path = input("请输入文件路径：")
-print(path)
-content = splitnovel('/home/liang/novel.txt')
+from chardet.universaldetector import UniversalDetector
 
-for i,j,k in os.walk('/home/liang/python'):
-    print("path:",i)
-    print('dirs:',j)
-    print('files:',k)
-'''
- 
-# -*- coding: utf-8 -*-
-# @Date     : 2018-11-02 17:38:53
-# @Author   : Jimy_Fengqi (jmps515@163.com)
-# @Link     : https://blog.csdn.net/qiqiyingse
-# @Version  : V1.0
- 
-'''
-将txt小说分割转换成单个章节文件
-文件名字以章节命名
-本文运行在python3上面，
-处理小说的时候，需要将小说的格式以utf-8保存
-（处理以ANSI编码格式的txt文本会出现错误）
-'''
- 
-import re
-import os
-import sys
- 
-# txt book's path.
-novel_name='' #小说名字
-source_path = input("请输入小说的绝对路径：")
- 
-path_pieces = os.path.split(source_path)
-print(path_pieces)
-novel_title = re.sub(r'\.(txt)$', '', path_pieces[1])
-target_path = '%s/%s' % (path_pieces[0], novel_title)#小说分章目录
-section_re = re.compile(r'^第.+章.*$')
+def get_encoding(file_path):
+    detector = UniversalDetector()
+    with open(file_path, 'rb') as file:
+        for line in file:
+            detector.feed(line)
+            if detector.done: break
+    detector.close()
+    return detector.result['encoding']
 
+def split_novel(file_path):
+    ''' 
+    将指定小说文本按章节分割，并分别保存为新的文本文件。
+    :param file_path: 小说文件的路径 
+    '''
+    file_encoding = get_encoding(file_path)
+    # 获取小说名称和所在目录
+    dir_path, full_name = os.path.split(file_path)
+    novel_name, _ = os.path.splitext(full_name)
+    target_dir = os.path.join(dir_path, novel_name)
+    # 设置章节文件保存目录
+    # 如果章节文件保存目录不存在，则创建该目录
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    
+    # 正则表达式匹配章节标题
+    section_pattern = re.compile(r'第.+章.*')
+    sec_count = 0  # 章节计数
+    sec_cache = []  # 存放当前章节内容
+    title_cache = []  # 存放所有章节标题，用于生成目录文件
+    
+    # 读取原小说内容，按章节分割并保存到新文件中
+    with open(file_path, 'r', encoding=file_encoding,errors='replace') as input_file:
+        chapter_title = "Chapter_Start"  # Default title or empty string
+        for line in input_file:
+            if re.match(section_pattern, line):
+                # Now, if this is not the first chapter,
+                # `chapter_title` will already have a sensible value.
+                if sec_cache:
+                    save_chapter(sec_count, sec_cache, target_dir, chapter_title)
+                sec_count += 1
+                chapter_title = re.sub(r'[\s~*+?,?，？]+', '_', line.strip())
+                title_cache.append(chapter_title + '\n')
+                sec_cache = [line]
+            else:
+                sec_cache.append(line)
+        # After the loop to save the last chapter
+        if sec_cache:
+            save_chapter(sec_count, sec_cache, target_dir, chapter_title)
+    
+    # 生成目录文件
+    generate_contents(title_cache, target_dir, novel_name)
+    print('完成。共处理 %d 章。' % sec_count)
 
-def main():
-    print(path_pieces)
-    if not os.path.exists(target_path):
-        os.mkdir(target_path)
-    output = open('%s/前言.txt' % (target_path), 'w',encoding='utf-8')
-    preface_title = '%s 前言' % novel_title
-    output.writelines(preface_title)
+def save_chapter(sec_count, sec_cache, target_dir, chapter_title):
+    ''' 
+    保存单独的章节为文本文件。
+    :param sec_count: 章节编号
+    :param sec_cache: 章节内容
+    :param target_dir: 目标文件夹路径
+    :param chapter_title: 章节标题 
+    '''
+    chapter_file_path = os.path.join(target_dir, f'{sec_count:03d}_{chapter_title}.txt')  # 构建文件名，包括章节编号
+    with open(chapter_file_path, 'w', encoding='utf-8') as f:
+        f.writelines(sec_cache)
 
-    input = open(source_path, 'r',encoding='utf-8')
-    sec_count = 0
-    sec_cache = []
-    title_cache=[]
-
-
-    for line in input:
-        if re.match(section_re,line):
-            line = re.sub(r'\s+','',line)
-            print('转换中... %s...' % line)
-            output.writelines(sec_cache)
-            output.flush()
-            output.close()
-            sec_cache = []
-            sec_count +=1
-            chapter_name=re.sub('(~+|\*+|\,+|\?+|\，+|\?+|\d+)','_',line)
-            output = open('%s/%s.txt' % (target_path, chapter_name), 'w',encoding='utf-8')
-            output.writelines(line)
-            title_cache.append(line+'\n')
-        else:
-            sec_cache.append(line)
-    output.writelines(sec_cache)
-    output.flush()
-    output.close()
-    sec_cache = []
-
-    output = open('%s\\目录.txt' % (target_path), 'w',encoding='utf-8')
-    menu_head = '%s 目录' % novel_title
-    output.writelines(menu_head)
-    output.writelines(title_cache)
-    output.flush()
-    output.close()
-    inx_cache = []
-
-    print ('completed. %d chapter(s) in total.' % sec_count)
-
+def generate_contents(title_cache, target_dir, novel_name):
+    ''' 
+    根据收集到的章节标题生成目录文件。
+    :param title_cache: 章节标题列表
+    :param target_dir: 目标文件夹路径
+    :param novel_name: 小说名字 
+    '''
+    contents_file_path = os.path.join(target_dir, '目录.txt')
+    with open(contents_file_path, 'w', encoding='utf-8') as f:
+        f.write(novel_name + ' 目录\n')
+        f.writelines(title_cache)
 
 if __name__ == '__main__':
-    main()
+    source_path = input("请输入小说的绝对路径：")
+    split_novel(source_path)
--- wsl/shell/new_sync.sh
+++ wsl/shell/new_sync.sh
@@ -1,15 +1,25 @@
-
+# 定义输入参数: 源文件夹路径和目标文件夹路径
 file_path=$1
 tofile_path=$2
-function nsync(){
-	file=$(find ${file_path}/* -newer ${file_path}/temp_file)
-	if [[ -z ${file} ]];then
-		echo "目标文件夹内无更新文件"
-		touch ${file_path}/temp_file
-	else	
-		echo -e "更新文件为\n${file}"
-		cp -r ${file} ${tofile_path}
-		touch ${file_path}/temp_file
-	fi
+
+# 定义文件同步函数nsync
+function nsync() {
+  # 使用find命令查找比temp_file更新的文件或文件夹
+  file=$(find ${file_path}/* -newer ${file_path}/temp_file)
+
+  # 判断是否找到新文件
+  if [[ -z ${file} ]]; then
+    echo "目标文件夹内无更新文件"
+    # 没有找到新文件时，更新temp_file的时间戳
+    touch ${file_path}/temp_file
+  else
+    echo -e "更新文件为\n${file}"
+    # 将找到的新文件或文件夹复制到目标文件夹
+    cp -r ${file} ${tofile_path}
+    # 更新temp_file的时间戳
+    touch ${file_path}/temp_file
+  fi
 }
+
+# 调用nsync函数执行同步
 nsync
